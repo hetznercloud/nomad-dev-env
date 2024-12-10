158f40ebca67d23216e749facc756af0f150f01f
lukasmetzner <lukas.metzner@hetzner-cloud.de>
2024-12-10 16:35:54 +0100

    chore: set example version ref to v0.0.0

--- example/main.tf
+++ example/main.tf
@@ -1,6 +1,5 @@
 module "dev" {
-  # source = "github.com/hetznercloud/nomad-dev-env?ref=v0.0.0"
-  source = "../"
+  source = "github.com/hetznercloud/nomad-dev-env?ref=v0.0.0" # x-releaser-pleaser-version
 
   hcloud_token = var.hcloud_token
 }
